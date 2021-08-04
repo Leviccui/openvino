c5dbb8e5210ca23c5ebfc5480d6569ead5fde648
Leviccui <cui@LAPTOP-1OOF6LP5.localdomain>
2021-08-04 20:15:24 +0800

    first commit

--- make.sh
+++ make.sh
@@ -1,1 +1,11 @@
-cd build/inference-engine/thirdparty/clDNN && make -j8 && cd -
+cd ~/openvino/inference-engine/thirdparty/clDNN
+#build
+if [ $2 -eq 1 ];
+then
+	echo "#define PARAM "$1 > ~/openvino/inference-engine/thirdparty/clDNN/tests/test_cases/PARAM.h
+	echo "#define CHECK_RESULTS" >> ~/openvino/inference-engine/thirdparty/clDNN/tests/test_cases/PARAM.h
+else
+	echo "#define PARAM "$1 > ~/openvino/inference-engine/thirdparty/clDNN/tests/test_cases/PARAM.h
+fi
+
+cd ~/openvino/build/inference-engine/thirdparty/clDNN && make -j8 && cd -
