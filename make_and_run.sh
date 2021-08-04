3b6bc0aef56598ec165cb7c39b1e0e9eb47874ef
Leviccui <cui@LAPTOP-1OOF6LP5.localdomain>
2021-08-04 12:59:50 +0800

    first commit

--- make_and_run.sh
+++ make_and_run.sh
@@ -12,5 +12,4 @@ cd ~/openvino/build/inference-engine/thirdparty/clDNN && make -j8 && cd ..
 
 #run
 #params=("14_14_512" "28_28_256" "28_28_512" "56_56_128" "56_56_256" "112_112_64" "112_112_128" "224_224_3" "224_224_64")
-build/out/Linux64/Release/tests64 --gtest_filter=my_convolution_gpu.my_vgg16_2dconv
-build/out/Linux64/Release/tests64 --gtest_filter=my_convolution_gpu.cldnn_vgg16_2dconv
+~/openvino/bin/intel64/Release/clDNN_unit_tests64 --gtest_filter=my_convolution_gpu.cldnn_vgg16_2dconv
