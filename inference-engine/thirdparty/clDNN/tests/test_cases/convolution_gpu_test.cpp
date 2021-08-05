836ff840679bf43f18195da2b422a2c04a60158c
Leviccui <cui@LAPTOP-1OOF6LP5.localdomain>
2021-08-05 14:40:20 +0800

    add host timer

--- inference-engine/thirdparty/clDNN/tests/test_cases/convolution_gpu_test.cpp
+++ inference-engine/thirdparty/clDNN/tests/test_cases/convolution_gpu_test.cpp
@@ -376,7 +376,7 @@ void cldnn_vgg16_test() {
     network network(engine, topology, options);
     network.set_input_data("input", input_mem);
 
-#include <sys/time.h>
+#include <unistd.h>
 #include<stdio.h>
     struct timeval start, end;
     int timeuse;
